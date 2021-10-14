0476afe8fec65697bee9d00ee81a93d8292b6216
Hrithik MJ <31767239+HrithikMJ@users.noreply.github.com>
2021-10-14 12:14:11 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,69 @@
 import { useState,useEffect } from "react";
 import './App.css'
+import { Switch, Route, useHistory, useLocation , Redirect } from "react-router-dom";
 import Login from "./Pages/Login";
-function App() {
-  const [name,setName] = useState("")
-  const [age,setAge] = useState("")
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
 
-  useEffect(()=>{
-    fetch('http://127.0.0.1:8000/',{
-      method:'GET',
-      headers:{
-        'Content-type':'application/json'
-      }
-    }).then((res)=>
-    { 
-      return res.json()
-    }).then((res)=> {
-      res.forEach(element => {
-       setName(element.name)
-       setAge(element.age)
-      });
-    })
-  },[])
+function App() {
+
+  const [name,setName] = useState("")
+  const [auth,setAuth] = useState(false)
+  const history = useHistory();
+  const location = useLocation();
+  // useEffect(() => {
+  //   if ( auth) {
+  //     history.push( "login");
+  //   }
+  //   if (location.pathname === "/home") {
+  //     setAuth(true);
+  //     history.push("/home");
+  //   }
+  // });
+  //const location = useLocation();
+//   useEffect(()=>{
+//     fetch('http://127.0.0.1:8000/',{
+//       method:'GET',
+//       headers:{
+//         'Content-type':'application/json'
+//       }
+//     }).then((res)=>
+//     { 
+//       return res.json()
+//     }).then((res)=> {
+//       res.forEach(element => {
+//        setName(element.name)
+//        setAge(element.age)
+//       });
+//     })
+//   },[])
   
-useEffect(()=>{
-  fetch('http://127.0.0.1:8000/getcaseview',{
-    method:"GET",
-    headers:{
-      'Content-type':'application/json'
-    }
-  }).then((res)=>{
-    return res.json()
-  }).then((res)=>{
-    res.forEach((res)=>console.log(res.case_number))
-  })
-},[])
+// useEffect(()=>{
+//   fetch('http://127.0.0.1:8000/getcaseview',{
+//     method:"GET",
+//     headers:{
+//       'Content-type':'application/json'
+//     }
+//   }).then((res)=>{
+//     return res.json()
+//   }).then((res)=>{
+//     res.forEach((res)=>console.log(res.case_number))
+//   })
+// },[])
 
 
   return (
-    <div className="App">
-   <Login />
-    </div>
+    <Switch>
+      <Redirect exact from="/" to="/login" />
+    <Route path="/login" component={Login} />
+    <>
+      <Navbar />
+      <Route exact path="/home" component={Home} />
+      
+      
+    </>
+  </Switch>
+   
   );
 }
 
--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,10 +1,23 @@
-
+import { Switch, Route, useHistory, useLocation } from "react-router-dom";
+import { useState } from "react";
+import "./Login.css";
 function Login() {
+  const history= useHistory();
 return (
     <div>
-    <h1 onClick={() => {
-        console.log("Hello wd");}}>hello</h1>
-      </div>  
+    <div className="loginMC"> {/*main container*/}
+      <div className="loginSC">{/*sub container*/}
+        <div className="login">
+          <form >
+                    <input type="text"></input>
+                    <button type="submit" onClick={() => {
+                    history.push("home");
+                  }} > click</button>
+                  </form>
+        </div>
+      </div>
+    </div>
+    </div>
   );
 
     }
