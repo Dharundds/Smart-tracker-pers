c8f04887e7c588bbcc31725e61f1eb47bc166bfa
Hrithik MJ <31767239+HrithikMJ@users.noreply.github.com>
2021-11-08 15:40:49 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -20,45 +20,6 @@ function App() {
   const [auth, setAuth] = useState(false);
   const history = useHistory();
   const location = useLocation();
-  // useEffect(() => {
-  //   if ( auth) {
-  //     history.push( "login");
-  //   }
-  //   if (location.pathname === "/home") {
-  //     setAuth(true);
-  //     history.push("/home");
-  //   }
-  // });
-  //const location = useLocation();
-  //   useEffect(()=>{
-  //     fetch('http://127.0.0.1:8000/',{
-  //       method:'GET',
-  //       headers:{
-  //         'Content-type':'application/json'
-  //       }
-  //     }).then((res)=>
-  //     {
-  //       return res.json()
-  //     }).then((res)=> {
-  //       res.forEach(element => {
-  //        setName(element.name)
-  //        setAge(element.age)
-  //       });
-  //     })
-  //   },[])
-
-  // useEffect(()=>{
-  //   fetch('http://127.0.0.1:8000/getcaseview',{
-  //     method:"GET",
-  //     headers:{
-  //       'Content-type':'application/json'
-  //     }
-  //   }).then((res)=>{
-  //     return res.json()
-  //   }).then((res)=>{
-  //     res.forEach((res)=>console.log(res.case_number))
-  //   })
-  // },[])
 
   return (
     <Switch>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,16 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useHistory, useLocation } from "react-router";
 import Symbols from "../../Components/Symbols";
 import CaseViews from "../../Components/CaseViews";
 import PEviews from "../../Components/PEviews/PEviews";
 import "./Home.css";
 function Home() {
-  const history = useHistory();
-  const location = useLocation();
-
-  if (localStorage.getItem("myData") == null) {
-    localStorage.setItem("myData", location.state.name);
-  }
   const [view, setView] = useState("Accounts View");
   const [username, setUsername] = useState(localStorage.getItem("myData"));
 
