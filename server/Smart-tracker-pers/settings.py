b43d126cd717beb111192fa466d98bd866d255cd
Dharundds <76607616+Dharundds@users.noreply.github.com>
2021-11-11 11:19:26 +0530

    update

--- server/Smart-tracker-pers/settings.py
+++ server/Smart-tracker-pers/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
-ROOT_URLCONF = 'SmartTracker.urls'
+ROOT_URLCONF = 'Smart-tracker-pers.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'SmartTracker.wsgi.application'
+WSGI_APPLICATION = 'Smart-tracker-pers.wsgi.application'
 
 
 # Database
